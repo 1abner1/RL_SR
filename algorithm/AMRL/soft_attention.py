60f15222fac68a5e14ace6473016fe6d5eb409aa
“1abner1” <axiaowwa@163.com>
2022-05-08 09:51:57 +0800

    修改注意力机制

--- algorithm/AMRL/soft_attention.py
+++ algorithm/AMRL/soft_attention.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import numpy as np
 import torch.nn.functional as F
 import torch.optim as optim
+from math import sqrt
+import torch
+import torch.nn as nn
 
 class MLP(nn.Module):#自定义类 继承nn.Module
 
@@ -22,6 +25,46 @@ class MLP(nn.Module):#自定义类 继承nn.Module
         x = self.model(x)
         #x传入自定义的model类 返回经过模型后的输出
         return x
+
+
+
+class MultiHeadSelfAttention(nn.Module):
+    dim_in: int  # input dimension
+    dim_k: int   # key and query dimension
+    dim_v: int   # value dimension
+    num_heads: int  # number of heads, for each head, dim_* = dim_* // num_heads
+
+    def __init__(self, dim_in, dim_k, dim_v, num_heads=8):
+        super(MultiHeadSelfAttention, self).__init__()
+        assert dim_k % num_heads == 0 and dim_v % num_heads == 0, "dim_k and dim_v must be multiple of num_heads"
+        self.dim_in = dim_in
+        self.dim_k = dim_k
+        self.dim_v = dim_v
+        self.num_heads = num_heads
+        self.linear_q = nn.Linear(dim_in, dim_k, bias=False)
+        self.linear_k = nn.Linear(dim_in, dim_k, bias=False)
+        self.linear_v = nn.Linear(dim_in, dim_v, bias=False)
+        self._norm_fact = 1 / sqrt(dim_k // num_heads)
+
+    def forward(self, x):
+        # x: tensor of shape (batch, n, dim_in)
+        batch, n, dim_in = x.shape
+        assert dim_in == self.dim_in
+
+        nh = self.num_heads
+        dk = self.dim_k // nh  # dim_k of each head
+        dv = self.dim_v // nh  # dim_v of each head
+
+        q = self.linear_q(x).reshape(batch, n, nh, dk).transpose(1, 2)  # (batch, nh, n, dk)
+        k = self.linear_k(x).reshape(batch, n, nh, dk).transpose(1, 2)  # (batch, nh, n, dk)
+        v = self.linear_v(x).reshape(batch, n, nh, dv).transpose(1, 2)  # (batch, nh, n, dv)
+
+        dist = torch.matmul(q, k.transpose(2, 3)) * self._norm_fact  # batch, nh, n, n
+        dist = torch.softmax(dist, dim=-1)  # batch, nh, n, n
+
+        att = torch.matmul(dist, v)  # batch, nh, n, dv
+        att = att.transpose(1, 2).reshape(batch, n, self.dim_v)  # batch, n, dim_v
+        return att
 
 # # 输入参数
 # s1 =np.random.randn(1,784)
