8976052330bc10cfdd469fec20fa482123361aea
abner <axiaowwa@163.com>
2022-05-08 10:33:30 +0800

    修改外部任务池

--- algorithm/AMRL/amrl_run.py
+++ algorithm/AMRL/amrl_run.py
@@ -26,41 +26,46 @@ def get_args():
 def ada_meta_rl():
     meta_learn = ml.MetaLearner()
 
+def outloop_select_task():
+    task_pool = {"task1": "D:/Pytorch_RL_SR/algorithm\AMRL/task1/car_seg_avoid.exe",
+                 "task2": "D:/Pytorch_RL_SR/algorithm\AMRL/task2/car_seg_avoid.exe",
+                 "task3": "D:/Pytorch_RL_SR/algorithm\AMRL/task3/car_seg_avoid.exe",
+                 "task4": "D:/Pytorch_RL_SR/algorithm\AMRL/task4/car_seg_avoid.exe",
+                 "task5": "D:/Pytorch_RL_SR/algorithm\AMRL/task5/car_seg_avoid.exe",
+                 "task6": "D:/Pytorch_RL_SR/algorithm\AMRL/task6/car_seg_avoid.exe",
+                 "task7": "D:/Pytorch_RL_SR/algorithm\AMRL/task7/car_seg_avoid.exe",
+                 "task8": "D:/Pytorch_RL_SR/algorithm\AMRL/task8/car_seg_avoid.exe",
+                 "task9": "D:/Pytorch_RL_SR/algorithm\AMRL/task9/car_seg_avoid.exe",
+                 "task10": "D:/Pytorch_RL_SR/algorithm\AMRL/task10/car_seg_avoid.exe",
+                 "task11": "D:/Pytorch_RL_SR/algorithm\AMRL/task11/car_seg_avoid.exe",
+                 "task12": "D:/Pytorch_RL_SR/algorithm\AMRL/task12/car_seg_avoid.exe",
+                 "task13": "D:/Pytorch_RL_SR/algorithm\AMRL/task13/car_seg_avoid.exe",
+                 "task14": "D:/Pytorch_RL_SR/algorithm\AMRL/task14/car_seg_avoid.exe",
+                 "task15": "D:/Pytorch_RL_SR/algorithm\AMRL/task15/car_seg_avoid.exe",
+                 "task16": "D:/Pytorch_RL_SR/algorithm\AMRL/task16/car_seg_avoid.exe",
+                 "task17": "D:/Pytorch_RL_SR/algorithm\AMRL/task17/car_seg_avoid.exe",
+                 "task18": "D:/Pytorch_RL_SR/algorithm\AMRL/task18/car_seg_avoid.exe",
+                 "task19": "D:/Pytorch_RL_SR/algorithm\AMRL/task19/car_seg_avoid.exe",
+                 "task20": "D:/Pytorch_RL_SR/algorithm\AMRL/task20/car_seg_avoid.exe",
+                 "task21": "D:/Pytorch_RL_SR/algorithm\AMRL/task21/car_seg_avoid.exe",
+                 "task22": "D:/Pytorch_RL_SR/algorithm\AMRL/task22/car_seg_avoid.exe",
+                 "task23": "D:/Pytorch_RL_SR/algorithm\AMRL/task23/car_seg_avoid.exe",
+                 "task24": "D:/Pytorch_RL_SR/algorithm\AMRL/task24/car_seg_avoid.exe",
+                 "task25": "D:/Pytorch_RL_SR/algorithm\AMRL/task25/car_seg_avoid.exe",
+                 "task26": "D:/Pytorch_RL_SR/algorithm\AMRL/task26/car_seg_avoid.exe",
+                 "task27": "D:/Pytorch_RL_SR/algorithm\AMRL/task27/car_seg_avoid.exe",
+                 "task28": "D:/Pytorch_RL_SR/algorithm\AMRL/task28/car_seg_avoid.exe",
+                 "task29": "D:/Pytorch_RL_SR/algorithm\AMRL/task29/car_seg_avoid.exe",
+                 "task30": "D:/Pytorch_RL_SR/algorithm\AMRL/task30/car_seg_avoid.exe"
+                 }
+    select_task = task_pool["task1"]
+
+    return select_task
+
 def meta_train():
    # 创建30种环境，创建一个字典一个数字对应一个任务
    # env = unity_wrapper.UnityWrapper(train_mode=True, base_port=5004,file_name=r"D:\Pytorch_RL_SR\algorithm\AMRL\testtask\car_seg_avoid.exe")
-   task_pool={"task1":"D:/Pytorch_RL_SR/algorithm\AMRL/task1/car_seg_avoid.exe",
-              "task2":"D:/Pytorch_RL_SR/algorithm\AMRL/task2/car_seg_avoid.exe",
-              "task3":"D:/Pytorch_RL_SR/algorithm\AMRL/task3/car_seg_avoid.exe",
-              "task4":"D:/Pytorch_RL_SR/algorithm\AMRL/task4/car_seg_avoid.exe",
-              "task5":"D:/Pytorch_RL_SR/algorithm\AMRL/task5/car_seg_avoid.exe",
-              "task6":"D:/Pytorch_RL_SR/algorithm\AMRL/task6/car_seg_avoid.exe",
-              "task7":"D:/Pytorch_RL_SR/algorithm\AMRL/task7/car_seg_avoid.exe",
-              "task8":"D:/Pytorch_RL_SR/algorithm\AMRL/task8/car_seg_avoid.exe",
-              "task9":"D:/Pytorch_RL_SR/algorithm\AMRL/task9/car_seg_avoid.exe",
-              "task10":"D:/Pytorch_RL_SR/algorithm\AMRL/task10/car_seg_avoid.exe",
-              "task11":"D:/Pytorch_RL_SR/algorithm\AMRL/task11/car_seg_avoid.exe",
-              "task12":"D:/Pytorch_RL_SR/algorithm\AMRL/task12/car_seg_avoid.exe",
-              "task13":"D:/Pytorch_RL_SR/algorithm\AMRL/task13/car_seg_avoid.exe",
-              "task14":"D:/Pytorch_RL_SR/algorithm\AMRL/task14/car_seg_avoid.exe",
-              "task15":"D:/Pytorch_RL_SR/algorithm\AMRL/task15/car_seg_avoid.exe",
-              "task16":"D:/Pytorch_RL_SR/algorithm\AMRL/task16/car_seg_avoid.exe",
-              "task17":"D:/Pytorch_RL_SR/algorithm\AMRL/task17/car_seg_avoid.exe",
-              "task18":"D:/Pytorch_RL_SR/algorithm\AMRL/task18/car_seg_avoid.exe",
-              "task19":"D:/Pytorch_RL_SR/algorithm\AMRL/task19/car_seg_avoid.exe",
-              "task20":"D:/Pytorch_RL_SR/algorithm\AMRL/task20/car_seg_avoid.exe",
-              "task21":"D:/Pytorch_RL_SR/algorithm\AMRL/task21/car_seg_avoid.exe",
-              "task22":"D:/Pytorch_RL_SR/algorithm\AMRL/task22/car_seg_avoid.exe",
-              "task23":"D:/Pytorch_RL_SR/algorithm\AMRL/task23/car_seg_avoid.exe",
-              "task24":"D:/Pytorch_RL_SR/algorithm\AMRL/task24/car_seg_avoid.exe",
-              "task25":"D:/Pytorch_RL_SR/algorithm\AMRL/task25/car_seg_avoid.exe",
-              "task26":"D:/Pytorch_RL_SR/algorithm\AMRL/task26/car_seg_avoid.exe",
-              "task27":"D:/Pytorch_RL_SR/algorithm\AMRL/task27/car_seg_avoid.exe",
-              "task28":"D:/Pytorch_RL_SR/algorithm\AMRL/task28/car_seg_avoid.exe",
-              "task29":"D:/Pytorch_RL_SR/algorithm\AMRL/task29/car_seg_avoid.exe",
-              "task30":"D:/Pytorch_RL_SR/algorithm\AMRL/task30/car_seg_avoid.exe"
-              }
-   print(task_pool["task1"])
+   pass
 
 
 def perceaction_image(state_image):
@@ -91,7 +96,7 @@ def main():
     # 获得参数信息
     parmater = get_args()
     # par1 = parmater("--task")
-    env = UnityWrapper(train_mode=True, base_port=5004)#,file_name=r"D:\Pytorch_RL_SR\algorithm\AMRL\testtask\car_seg_avoid.exe")
+    env = UnityWrapper(train_mode=True, base_port=5004,file_name=r"D:\RL_SR\envs\test\car_seg_avoid.exe")
     obs_shape_list, d_action_dim, c_action_dim = env.init()
     state_dim = obs_shape_list
     print("总的状态维度：",state_dim) #(前摄像头图像，左摄像头图像，右摄像头图像，射线数据，目标位置和速度)
@@ -103,6 +108,7 @@ def main():
 
     #train makeure task
     for episode in range(100):
+        #---------------------------感知层--------------------------------
         obs_list = env.reset()
         # 视觉信息
         forward_image = obs_list[0][0]
@@ -113,7 +119,7 @@ def main():
         #向量信息
         position = obs_list[4][0]
         #显示图片
-        Unity_image_show("forward-left-right",forward_image,left_image,right_image)
+        # Unity_image_show("forward-left-right",forward_image,left_image,right_image)
         #把图像数据提取特征变成一个8维的向量
         forward_image_deal_8v = perceaction_image(forward_image)
         left_image_deal_8v = perceaction_image(left_image)
@@ -127,7 +133,7 @@ def main():
         # 位置向量数据
         position_output = position_deal(position)
         print("位置向量输出",position_output)
-
+        #---------------------------感知层---------------------------------
         for step in range(2):
             # print("环境没有问题")
             pass
