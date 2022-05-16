d011355a987326655339a6a80c16f445aba5220c
1abner1 <axiaowwa@163.com>
2022-05-16 15:46:09 +0800

    "保存直方图"

--- plot/AMRL_data/bar.py
+++ plot/AMRL_data/bar.py
@@ -1,27 +1,27 @@
 import matplotlib.pyplot as plt
 
 
-labels = ['PPO', 'SAC', 'TRPO', 'A3C', 'DDPG', 'AMDDPG']
-map1 = [15, 24, 16, 27, 20,4]
-map2 = [25, 27, 20, 29, 25,5]
-map3 = [28, 30, 25, 32, 27,8]
+labels = ['MAML', 'PPO', 'PEAL', 'AMRL_PEP', 'AMRL_ICM', 'AMRL_Ours']
+trget_search_dis1 = [15, 24, 16, 27, 20,4]
+trget_search_dis2 = [25, 27, 20, 29, 25,5]
+trget_search_dis3 = [28, 30, 25, 32, 27,8]
 # map4 = [15, 25, 15, 30, 20,8]
-map1_std = [2, 2, 1, 1, 2,1]
-map2_std = [2, 2, 2, 2, 2,1]   
-map3_std = [2, 1, 2, 2, 2,1]
+trget_search_dis1_std = [2, 2, 1, 1, 2,1]
+trget_search_dis2_std = [2, 2, 2, 2, 2,1]
+trget_search_dis3_std = [2, 1, 2, 2, 2,1]
 
 width = 0.45       # the width of the bars: can also be len(x) sequence
 
 fig, ax = plt.subplots()
 
-cum =list(map(sum, zip(list(map1),list(map2))))
-ax.bar(labels, map1, width, yerr=map1_std, label='map1')
-ax.bar(labels, map2, width, yerr=map2_std,label='map2', bottom=map1)
-ax.bar(labels, map3, width, yerr=map3_std, label='map3',bottom=cum)
+cum =list(map(sum, zip(list(trget_search_dis1),list(trget_search_dis2))))
+ax.bar(labels, trget_search_dis1, width, yerr=trget_search_dis1_std, label='dis=30%')
+ax.bar(labels, trget_search_dis2, width, yerr=trget_search_dis2_std,label='dis=60%', bottom=trget_search_dis1)
+ax.bar(labels, trget_search_dis3, width, yerr=trget_search_dis3_std, label='dis=90%',bottom=cum)
 
 ax.set_ylabel('Number of collisions')
 ax.set_xlabel("Algorithm")
 # ax.set_title('Scores by group and gender')
 ax.legend()
-
+plt.savefig("Target search Avoid rate.pdf")
 plt.show()
