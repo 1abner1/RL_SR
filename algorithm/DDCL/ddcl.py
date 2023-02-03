01e159f161e8ff5160dd58261c1b0c9e57f5cd58
abner <axiaowenwen@163.com>
2023-02-03 16:43:52 +0800

    add ddcl algorithm

---
+++ algorithm/DDCL/ddcl.py
@@ -0,0 +1,28 @@
+#基于双重决策和先验知识的虚实迁移持续学习方法
+# 首先创建代码总体框架
+#感知阶段
+class Feature_Gain()
+     def SR_Fusion(self):
+        pass
+     def Graph_Bulid(self):
+        pass
+
+
+
+#决策阶段
+class Policy_Learning():
+    def actor(self):
+        pass
+    def critic(self):
+        pass
+class Rule_Control():
+      pass
+
+class Discriminator():
+      pass
+
+
+#执行阶段
+class Execution_Phase()
+    def main(self):
+        pass
